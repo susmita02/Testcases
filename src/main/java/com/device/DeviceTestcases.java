a509d4f94229ff40564d18449c17c14e18c614d5
susmita02 <susmita.panda02@gmail.com>
2019-08-21 08:57:53 +0530

    updated

--- src/main/java/com/device/DeviceTestcases.java
+++ src/main/java/com/device/DeviceTestcases.java
@@ -1,12 +1,17 @@
 package com.device;
 
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
@@ -27,7 +32,7 @@ public class DeviceTestcases {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(20000, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(40000, TimeUnit.SECONDS);
         driver.get("https://www.vzwdt.com/orbit");
 		
 	}
@@ -35,38 +40,57 @@ public class DeviceTestcases {
 	@Test
 	
 	public void loginPage() throws InterruptedException {
-		
-		driver.findElement(By.xpath("//input[@id='username']")).sendKeys("susmitaadm");
-		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("12345");
-		driver.findElement(By.xpath("//span[contains(text(),'Login')]")).click();
-		String homepagetitle =driver.getTitle();
-		System.out.println(homepagetitle);
-		
-		driver.findElement(By.xpath("//a[contains(@href,'device') and @class='container-item']")).click();
-		
-		Thread.sleep(30);
-//		WebElement header = driver.findElement(By.xpath("//li[@class='orbitAllPageTitle']"));
-		System.out.println("Device page displayed");
-		
-		WebElement element = driver.findElement(By.xpath("//button[@type = 'button' and @class = 'btn btn-primary']"));
 
-		JavascriptExecutor executor = (JavascriptExecutor)driver;
-		executor.executeScript("arguments[0].click()", element);
-		
-//		driver.findElement(By.xpath("//button[@type = 'button' and @class = 'btn btn-primary']")).click();
-		
-		WebElement adddevicedropdown = driver.findElement(By.xpath("//span[contains(text(),'Select Device Category')]"));
-		Select category = new Select(adddevicedropdown);
-		category.selectByVisibleText("Dark");
-		
-		driver.findElement(By.xpath("//button[contains(text(),'Add Device')]")).click();
-		
-		System.out.println(" Add device page opened");
-		
-	
-		
-		
-	}
+        
+
+        driver.findElement(By.xpath("//input[@id='username']")).sendKeys("susmitaadm");
+
+        driver.findElement(By.xpath("//input[@id='password']")).sendKeys("12345");
+
+        driver.findElement(By.xpath("//span[contains(text(),'Login')]")).click();
+
+       String homepagetitle =driver.getTitle();
+
+       System.out.println(homepagetitle);
+       driver.findElement(By.xpath("//a[contains(@href,'device') and @class='container-item']")).click();
+
+             Thread.sleep(30);
+       System.out.println("Device page displayed");
+
+
+       Set<String> handler = driver.getWindowHandles();
+       Iterator<String> it = handler.iterator();
+       String deviceList = it.next();
+       System.out.println("DeviceList window id "    +deviceList);
+
+       wait = new WebDriverWait(driver, 20);   // wait for 20 seconds
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.xpath("//button[@type = 'button' and @class = 'btn btn-primary']"))));
+       driver.findElement(By.xpath("//button[@type = 'button' and @class = 'btn btn-primary']")).click();
+       System.out.println("Add device pop up opened");
+      
+       
+       String devCat = it.next();
+       wait = new WebDriverWait(driver, 20);
+       driver.switchTo().window(devCat);
+       System.out.println("Devicecategory window id "    +devCat);
+       
+          // wait for 20 seconds
+       WebElement adddevicedropdown=   wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(By.cssSelector("#form-control ui-select-search ng-pristine ng-valid ng-touched"))));
+       Select select = new Select(adddevicedropdown);
+       try {
+       select.selectByVisibleText("Dark");
+       } catch (Exception e) {
+      e.printStackTrace();
+       }
+ 
+        driver.findElement(By.xpath("//button[contains(text(),'Add Device')]")).click();
+    
+       System.out.println(" Add device page opened");
+
+    
+      
+
+ }
 	
    @AfterMethod()
 	   
